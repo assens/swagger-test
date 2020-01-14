9415eb4260187173a8f17ac10a8a6728d16b3b68
Assen Sharlandjiev <assen.sharlandjiev@gmail.com>
2020-01-14 15:06:29 +0200

    ...

--- src/main/java/com/foo/test/swagger/dto/CustomerDTO.java
+++ src/main/java/com/foo/test/swagger/dto/CustomerDTO.java
@@ -1,10 +1,14 @@
 package com.foo.test.swagger.dto;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
 import java.time.ZonedDateTime;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -13,7 +17,22 @@ import lombok.Data;
 @ApiModel
 public class CustomerDTO {
   private String name;
+
+  @ApiModelProperty
   private ZonedDateTime zonedDateTime;
+
+  @ApiModelProperty
+  private OffsetDateTime offsetDateTime;
+
+  @ApiModelProperty
   private LocalDateTime localDateTime;
+
+  @ApiModelProperty
   private Instant instant;
+
+  @ApiModelProperty
+  private LocalDate localDate;
+
+  @ApiModelProperty(dataType = "java.time.OffsetDateTime")
+  private LocalTime localTime;
 }
